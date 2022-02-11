e69073f8db8b3a826f32f6952515f11e8664805f
pb10005 <atihsa.mihs.ne.ho.da@gmail.com>
2022-02-11 23:01:32 +0900

    Fix: Update script

--- set-alias.sh
+++ set-alias.sh
@@ -1,17 +1,17 @@
 function dn() {
-    docker compose run --rm -v $(pwd):/app dotnet-dev $@
+    docker compose run --rm --service-ports -v $(pwd):/app dotnet-dev $@
 }
 
 function vt() {
-    docker compose run --rm -v $(pwd):/app vite-dev $@
+    docker compose run --rm --service-ports -v $(pwd):/app vite-dev $@
 }
 
 function dotnet() {
-    docker compose run --rm -v $(pwd):/app dotnet-dev dotnet $@
+    docker compose run --rm --service-ports -v $(pwd):/app dotnet-dev dotnet $@
 }
 
 function yarn() {
-    docker compose run --rm -v $(pwd):/app vite-dev yarn $@
+    docker compose run --rm --service-ports -v $(pwd):/app vite-dev yarn $@
 }
 
 function npx() {
